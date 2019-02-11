a53d40267e4e14eae23ae39bf9f64eda85b62cab
cpelzer <christoph.pelzer@kisters.de>
2019-02-11 12:57:28 +0100

    dotfiles of git are now copied to the right location

--- git/install.sh
+++ git/install.sh
@@ -14,3 +14,27 @@ elif [[ ${OS} == CentOS ]]; then
 else
     echo "Git installation failed.. Couldn't figure out what system you use"
 fi
+
+echo "Now copying dotfiles"
+
+gitconfig=~/.gitconfig
+gitignoreglobal=~/.gitignore_global
+if [[ -e ${gitignoreglobal} ]]; then
+    echo "found ${gitignoreglobal}... Backing up"
+    cp -v ${gitignoreglobal} ${gitignoreglobal}.bak
+else
+   echo "No global gitignore found"
+fi
+
+if [[ -e ${gitconfig} ]]
+then
+    echo "${gitconfig} found... Backing up"
+    cp -v ${gitconfig} ${gitconfig}.bak
+else
+    echo "git config not found."
+fi
+
+echo "No copying new dotfiles"
+
+cp -v ${filepath}/dotfiles/.gitignore_global ${gitignoreglobal}
+cp -v ${filepath}/dotfiles/.gitconfig  ${gitconfig}
